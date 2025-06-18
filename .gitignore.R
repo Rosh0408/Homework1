5ad9705ac37fc084cf8ba1381d2a99f985eb6e83
Rosh0408 <roshg2001@gmail.com>
2025-06-18 17:37:12 +0400

    adding files to the repo

---
+++ .gitignore.R
@@ -0,0 +1,46 @@
+# History files
+.Rhistory
+.Rapp.history
+
+# Session Data files
+.RData
+.Ruserdata
+
+# RStudio files
+.Rproj.user/
+  *.Rproj
+
+# Environment files
+.Renviron
+.Rprofile
+
+# Temporary files
+*.o
+*.so
+*.dll
+*.exe
+
+# Sweave/knitr intermediate files
+*.aux
+*.log
+*.out
+*.toc
+*.nav
+*.snm
+*.vrb
+*.tex
+*.pdf
+
+# Quarto
+*_cache/
+  *_files/
+  *.nb.html
+*.html
+*.docx
+
+# Other
+.DS_Store
+Thumbs.db
+
+# Git
+.gitignore
